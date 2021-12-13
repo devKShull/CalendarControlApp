0e5da0fc9718b75d27893fabd4b77ee1aa6ef4fb
rhkstn0303 <rhkstn0303@naver.com>
2021-12-13 17:41:57 +0900

    delete

--- custom_modules/func/calRemoveFunc.js
+++
@@ -1,6 +0,0 @@
-import RNCalendarEvents from "react-native-calendar-events";
-
-export default calendarRemove = async (id) => {
-    const res = await RNCalendarEvents.removeCalendar(id)
-    return res
-}
--- custom_modules/func/eventRemoveFunc.js
+++
@@ -1,7 +0,0 @@
-import RNCalendarEvents from "react-native-calendar-events";
-
-export default eventRemoveFunc = async (id) => {
-    const res = await RNCalendarEvents.removeEvent(id)
-
-    return res
-}
